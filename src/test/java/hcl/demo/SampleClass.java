9359f5ec7ba9e85dfa611a8578ec898562222b00
jeyjra <jeyjeya234342@gmail.com>
2022-04-08 10:08:15 +0530

    trail

---
+++ src/test/java/hcl/demo/SampleClass.java
@@ -0,0 +1,49 @@
+package hcl.demo;
+
+public class SampleClass {
+
+	public void method1() {
+	
+	 System.out.println("method1");	
+		
+	}
+	public void method2() {
+		
+		 System.out.println("method2");	
+			
+		}
+	public void method3() {
+		
+		 System.out.println("method3");	
+			
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	  
+}
